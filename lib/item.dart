164f2e5868ef8ccf72d99ed31330f66a79cce0ea
amelia <meliamel.9977@gmail.com>
2022-12-16 01:19:11 +0700

    jobsheet 6

---
+++ lib/item.dart
@@ -0,0 +1,35 @@
+class Item {
+  late int id;
+  late String _itemId;
+  late String _name;
+  late int _price;
+  late int _stock;
+
+  Item(this._itemId, this._name, this._price, this._stock);
+
+  Item.map(dynamic obj) {
+    this._itemId = obj['kode_barang'];
+    this._name = obj['name'];
+    this._price = obj['price'];
+    this._stock = obj['stock'];
+  }
+
+  String get itemId => _itemId;
+  String get name => _name;
+  int get price => _price;
+  int get stok => _stock;
+
+  Map<String, dynamic> toMap() {
+    var map = <String, dynamic>{};
+
+    map['item_id'] = _itemId;
+    map['name'] = _name;
+    map['price'] = _price;
+    map['stok'] = _stock;
+    return map;
+  }
+
+  void setItemId(int id) {
+    this.id = id;
+  }
+}
---
+++ lib/pages/entry_form.dart
@@ -0,0 +1,171 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_sqlite/db_helper.dart';
+import 'package:flutter_sqlite/item.dart';
+
+class EntryForm extends StatefulWidget {
+  final Item _item;
+  final bool _isNew;
+
+  EntryForm(this._item, this._isNew);
+
+  @override
+  State<EntryForm> createState() => _EntryFormState();
+}
+
+class _EntryFormState extends State<EntryForm> {
+  late Item item;
+
+  final itemIdController = TextEditingController();
+  final nameController = TextEditingController();
+  final priceController = TextEditingController();
+  final stockController = TextEditingController();
+
+  Future insertData() async {
+    var db = DBHelper();
+    var item = Item(itemIdController.text, nameController.text,
+        int.parse(priceController.text), int.parse(stockController.text));
+    await db.saveItem(item);
+    print('Saved');
+  }
+
+  Future updateData() async {
+    var db = DBHelper();
+    var item = Item(itemIdController.text, nameController.text,
+        int.parse(priceController.text), int.parse(stockController.text));
+    item.setItemId(this.item.id);
+    await db.updateItem(item);
+  }
+
+  void _saveData() {
+    if (widget._isNew) {
+      insertData();
+    } else {
+      updateData();
+    }
+    Navigator.of(context).pop();
+  }
+
+  @override
+  void initState() {
+    if (widget._item != null) {
+      item = widget._item;
+      itemIdController.text = item.itemId;
+      nameController.text = item.name;
+      priceController.text = item.price.toString();
+      stockController.text = item.stok.toString();
+    }
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget._isNew ? 'Tambah' : 'Ubah'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.only(top: 15.0, left: 10.0, right: 10.0),
+        child: ListView(
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.only(top: 20.0, bottom: 20.0),
+              child: TextField(
+                controller: itemIdController,
+                keyboardType: TextInputType.text,
+                decoration: InputDecoration(
+                  labelText: 'Kode Barang',
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(5.0),
+                  ),
+                ),
+                onChanged: (value) {},
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 20.0, bottom: 20.0),
+              child: TextField(
+                controller: nameController,
+                keyboardType: TextInputType.text,
+                decoration: InputDecoration(
+                  labelText: 'Nama Barang',
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(5.0),
+                  ),
+                ),
+                onChanged: (value) {},
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 20.0, bottom: 20.0),
+              child: TextField(
+                controller: priceController,
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                  labelText: 'Harga',
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(5.0),
+                  ),
+                ),
+                onChanged: (value) {},
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 20.0, bottom: 20.0),
+              child: TextField(
+                controller: stockController,
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                  labelText: 'Stok',
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(5.0),
+                  ),
+                ),
+                onChanged: (value) {},
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 20.0, bottom: 20.0),
+              child: Row(
+                children: <Widget>[
+                  Expanded(
+                    child: RaisedButton(
+                      color: Colors.green,
+                      textColor: Colors.white,
+                      child: const Text(
+                        'Save',
+                        textScaleFactor: 1.5,
+                      ),
+                      onPressed: _saveData,
+                    ),
+                  ),
+                  Container(
+                    width: 5.0,
+                  ),
+                  Expanded(
+                    child: RaisedButton(
+                      color: Colors.red,
+                      textColor: Colors.white,
+                      child: const Text(
+                        'Cancel',
+                        textScaleFactor: 1.5,
+                      ),
+                      onPressed: () {
+                        Navigator.pop(context);
+                      },
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  RaisedButton(
+      {required Color color,
+      required Color textColor,
+      required Text child,
+      required void Function() onPressed}) {}
+}
